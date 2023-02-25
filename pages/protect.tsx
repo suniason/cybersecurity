c8ef00e523e8a7f06f3adb4bbb2498cb737d9cda
suniason <galvezbrett17@gmail.com>
2023-02-26 00:39:32 +0800

    new added

---
+++ pages/protect.tsx
@@ -0,0 +1,74 @@
+const Protect : React.FC = () => {
+    
+    return(
+        <>
+            <div className="wrapper">
+                <div className="wrapper">
+                    <div className="content1">
+                    <div className="gridtop">
+                        <img src="safetyonline.png" alt="Be safe online" />
+                        <div className="top">
+                            Do you know that in the Philippines, 
+                            there are already 73.9 million internet users and 92.05 million social media users 
+                            as of January 2022? That's 67% of the total population, which only shows that the 
+                            internet and social media are already a part of our daily lives. 
+                            But as we enjoy the benefits of this digital technology, we should also 
+                            be aware of its negative impacts, such as cyber threats and privacy violations. 
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="wrapper">
+                <div className="content2">
+                    <div className="bottom">
+                    Now, the big question is, how can we maximize the positive side of the internet 
+                    and social media without experiencing its negative consequences? Here are some 
+                    tips that you can follow to protect yourself online:
+                    </div>
+                    <div className="wrapper">
+                        <div className="gridbottom">
+                            <div className="grid1">
+                                <span>1</span><br />
+                                <b>Be mindful of oversharing.</b><br />
+                                Always be careful about the personal information you share online, 
+                                especially on social media platforms. Review and adjust your privacy 
+                                settings to limit the amount of personal information that others can see.
+                                
+                            </div>
+                            <div className="grid2">
+                                <span>2</span><br />
+                                <b>Use strong and unique passwords for each of your online accounts. </b><br />
+                                Also, avoid using the same password across multiple accounts. 
+                                You can also consider using a password manager to generate and store your passwords securely.
+                                
+                            </div>
+                            <div className="grid3">
+                                <span>3</span><br />
+                                <b>Always be cautious of suspicious emails, links, and messages from unknown sources.</b> <br />
+                                Do not click on any suspicious links or download attachments from unsolicited emails.
+                                
+                            </div>
+                            <div className="grid4">
+                                <span>4</span><br />
+                                <b>Beware of phishing scams that ask for your personal or financial information. </b><br />
+                                Verify the legitimacy of the request before providing any information.
+                            </div>
+                            <div className="grid5">
+                                <span>5</span><br />
+                                <b>Regularly backup your data.</b><br />
+                                This is to protect against data loss due to cyberattacks or other incidents.
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                @LearningTeam4
+            </div>
+        </>
+    )
+}
+
+export default Protect
